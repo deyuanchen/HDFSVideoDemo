1e43c5d4b2ac1bf0e8795f70c0c1c9195c9b0791
deyuanchen <321097355@qq.com>
2017-05-31 10:00:56 +0800

    commit

---
+++ src/main/java/com/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.constants;
+/**
+ * <p>Tiltle: com.constants </p>
+ * <p>Description: TODO(这里来描述信息) </p>
+ *
+ * @Author 陈德元
+ * @data: 2017-05-31
+ * @version: 1.0
+ */
+public class Constants {
+public static String NamenodeIP="125.216.242.200";//�˴���д���ڵ�IP��ַ
+public static String NamenodePort="9000";
+public static String HDFSAddress="hdfs://"+NamenodeIP+":"+NamenodePort;
+}
